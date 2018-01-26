b3bd3d189ca51c9b22191de5b4dfe532f0a6af05
Kevin LEVRON <kevin.levron@univ-pau.fr>
2018-01-26 17:48:46 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,29 +1,9 @@
 import 'es6-promise/auto';
 import Vue from 'vue';
-import Vuex from 'vuex';
-import VueRouter from 'vue-router';
 import App from './components/App.vue';
-import Default from './components/Default.vue';
 
-/*
- * Init Vuex
- */
-Vue.use(Vuex);
-const store = new Vuex.Store({
-    state: {
-    }
-});
-
-/*
- * Init VueRouter
- */
-Vue.use(VueRouter);
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        { path: '/', component: Default }
-    ]
-});
+import store from './store';
+import router from './router';
 
 /*
  * Init Vue
